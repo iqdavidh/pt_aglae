26f191c76a15f7a46ef5380d0c39c976eddf3970
davidh <iq.davidh@gmail.com>
2019-10-25 12:16:50 -0500

    regrese al de courreincas porque pense en el case

--- src/countPeaks.js
+++ src/countPeaks.js
@@ -1,5 +1,22 @@
+/*
+* 3. Given an array of strictly positive integers, determine the number of elements that are greater than
+the adjacent elements (further known as peaks). If an adjacent element is an equal value, it would only
+count as a peak if the next divergent number is smaller than the current element. Assume that there are
+0 values outside of the bounds of the array, which may lead to a peak. e.g. [1, 2, 2, 3, 4, 3, 5, 3, 2, 1, 2,
+3, 3, 3, 2, 2, 1, 3] can be thought of as 0, [1, 2, 2, 3, 4, 3, 5, 3, 2, 1, 2, 3, 3, 3, 2, 2, 1, 3], 0 and would
+thus have the parenthesized peaks [1, 2, 2, 3, (4), 3, (5), 3, 2, 1, 2, (3, 3, 3), 2, 2, 1, (3)]
+* */
+const countPeaks =(lista)=>{
 
-const countPeaks =()=>{
+  //TODO validar que es un array lo que se va a procesar y tiene mas de 1 elemento
+  //sin validación de argumentos porquye ya no hay tiempo
+
+  for(let i=0; i<lista.length; i++){
+
+  }
+
+  return respuesta="";
+
 
 };
 
--- test/countPeaks.test.js
+++ test/countPeaks.test.js
@@ -1,56 +1,16 @@
 const assert = require('assert');
-const countOccurences = require('../src/countOccurences');
+const countPeaks = require('../src/countPeaks');
 
-const fnSort = (a, b) => {
-  if (a.letra === b.letra) {
-    return 0
-  }
-
-  return a.letra > b.letra;
-};
-const compararLista = (lista1, lista2) => {
-
-
-  lista1.sort(fnSort);
-  lista2.sort(fnSort);
-
-  return JSON.stringify(lista1) === JSON.stringify(lista2);
-};
 
 describe('countOccurences', function () {
 
   it('should ok - caso basico sin espacios', function () {
-    let respuesta = countOccurences("Hello");
 
-    const respuestaEsperada = [{letra: "H", q: 1}, {letra: "e", q: 1}, {letra: "l", q: 2}, {letra: "o", q: 1}];
-    var ope = compararLista(respuesta, respuestaEsperada);
-    assert(ope);
-  });
+    let respuesta = countPeaks([1, 2, 2, 3, 4, 3, 5, 3, 2, 1, 2, 3, 3, 3, 2, 2, 1, 3]);
 
-  it('should ok - caso basico con espacios y no letras', function () {
-    let respuesta = countOccurences("H e l l o {,.");
+    const respuestaEsperada = "[1,2,2,3,(4),3,(5),3,2,1,2,(3, 3, 3),2,2,1,(3)]";
 
-    const respuestaEsperada = [{letra: "H", q: 1}, {letra: "e", q: 1}, {letra: "l", q: 2}, {letra: "o", q: 1}];
-    var ope = compararLista(respuesta, respuestaEsperada);
-    assert(ope);
-  });
-
-
-  it('should ok - caso basico pero letras inverso - para probar el comparador del test', function () {
-    let respuesta = countOccurences("olleH");
-
-    const respuestaEsperada = [{letra: "H", q: 1}, {letra: "e", q: 1}, {letra: "l", q: 2}, {letra: "o", q: 1}];
-    var ope = compararLista(respuesta, respuestaEsperada);
-    assert(ope);
-  });
-
-
-  it('should false - caso no coincie case', function () {
-    let respuesta = countOccurences("hello");
-
-    const respuestaEsperada = [{letra: "H", q: 1}, {letra: "e", q: 1}, {letra: "l", q: 2}, {letra: "o", q: 1}];
-    var ope = compararLista(respuesta, respuestaEsperada);
-    assert(!ope);
+    assert(respuesta === respuestaEsperada);
   });
 
 });
